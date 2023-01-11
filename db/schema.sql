33a7f185a3381e0762dc71f9d33333043ddd99a2
saramangelo <saramangelo@gmail.com>
2023-01-10 22:05:44 -0800

    prompt questions and promise psuedocoded

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,52 @@
+
+DROP DATABASE IF EXISTS workplace_db;
+CREATE DATABASE workplace_db;
+
+USE workplace_db;
+
 -- department
-
 -- id: INT PRIMARY KEY
-
 -- name: VARCHAR(30) to hold department name
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
 
 
 -- role
-
 -- id: INT PRIMARY KEY
-
 -- title: VARCHAR(30) to hold role title
-
 -- salary: DECIMAL to hold role salary
-
 -- department_id: INT to hold reference to department role belongs to
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) 
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
 
 
 -- employee
-
 -- id: INT PRIMARY KEY
-
 -- first_name: VARCHAR(30) to hold employee first name
-
 -- last_name: VARCHAR(30) to hold employee last name
-
 -- role_id: INT to hold reference to employee role
-
 -- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  FOREIGN KEY (role_id) 
+  REFERENCES role(id)
+  ON DELETE SET NULL
+  manager_id INT,
+  FOREIGN KEY (manager_id) 
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
+
+
--- index.js
+++ index.js
@@ -3,6 +3,50 @@ const consoleTable = require('console.table')
 const mysql = require('mysql2');
 const db = require('./db/connection');
 
+// inquirer to prompt in command line
+
+// array of questions
+const options = [
+    {
+      type: "list",
+      message: "Please select from the list what would you like to do:",
+      choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
+      name: "optionSelection",
+    },
+];
+
+function askPromptQuestions() {
+  inquirer
+    .prompt(options)
+
+    // data = giant object (answers from input)
+    .then((answers) => {
+      // console.log(answers);
+      if (answers.optionSelection === "View all departments") {
+        promptViewAllDepartments();
+      }
+      if (answers.optionSelection === "View all roles") {
+        promptViewAllRoles();
+      }
+      if (answers.optionSelection === "View all employees") {
+        promptViewAllEmployees();
+      }
+      if (answers.optionSelection === "Add a department") {
+        promptAddDepartment();
+      }
+      if (answers.optionSelection === "Add a role") {
+        promptAddRole();
+      }
+      if (answers.optionSelection === "Add an employee") {
+        promptAddEmployee();
+      }
+      if (answers.optionSelection === "Update an employee role") {
+        promptUpdateEmployeeRole();
+      }
+    });
+  }
+
+  // these functions above are not correct, I need to update with either more prompts or a db.query to CRUD db
 
 
 
