9365845d0cf0ba84c0d6802e178ad6eb18c5d8cf
saramangelo <saramangelo@gmail.com>
2023-01-17 22:47:46 -0800

    fixed view all employees SQL statement

--- Queries.js
+++ Queries.js
@@ -25,11 +25,12 @@ class Queries {
 
   viewAllEmployees() {
     // this needs to present a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-    return this.connection.promise().query(`SELECT employee.id, employee.first_name, employee.last_name, manager.first_name AS manager, role.title, role.salary FROM employee
-    LEFT JOIN employee manager ON manager.id = employee.manager_id
-    LEFT JOIN role
-    ON employee.role_id = role.id;`);
+    return this.connection.promise().query(
+
+    `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;`
+    );
   }
+
 
   addDepartment(departmentName) {
     // this needs to have user be prompted to enter the name of the department and that department is added to the database
